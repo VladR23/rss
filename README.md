7cabf2076f90b1c9237669c4299ea95acccc202b
Vlad Rudenua <rudenua.dev@gmail.com>
2022-11-30 11:08:36 +0300

    init
    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+simple console app, which help to pars rss news and send them to telegram bot 
---
+++ bin/rssp.dart
@@ -0,0 +1,5 @@
+import 'package:rssp/rssp.dart' as rssp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rssp.calculate()}!');
+}
